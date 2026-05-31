c2ca7624125b38c9031250ea603beb64ca99d209
Dennis EJ <albereli17@hotmail.com>
2026-05-31 04:50:49 -0600

    Tarea-1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter.component'; //Se agrega la importación automáticamente
+import { HeroPageComponent } from './pages/hero/hero-page.component'
 
 export const routes: Routes = [
     {
         path: '', //se utiliza vacío sin espacio entre las comillas para que sea la ruta raiz
         component: CounterPageComponent //Con el path vacío, se carga la URL inicial (raiz)
+    },
+    {
+        path: 'hero', //Se agrega el path para localHost:4200/hero
+        component: HeroPageComponent //muestra el HeroPage con el path en la url
     }
 ];
