d515d0e6bc856a363d60f15727789a783edb3dae
Dennis EJ <albereli17@hotmail.com>
2026-06-07 10:18:50 -0600

    Semana-4

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,46 +1,35 @@
-import {Component, signal} from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 
-@Component ({
-selector: 'app-hero-page',
-
-//importa el html y css del hero
-templateUrl: './hero-page.component.html',
-styleUrl: './hero-page.component.css'
+@Component({
+    // importa el html y css del hero
+    templateUrl: './hero-page.component.html',
+    styleUrl: './hero-page.component.css'
 })
 
 export class HeroPageComponent {
-    name = 'DeadPool';
-    age = 33;
 
-    heroDescription = signal(
-        `Su nombre es ${this.name} y tiene ${this.age} años`
+    name = signal('DeadPool');
+    age = signal(33);
+
+    heroDescription = computed(() => //cambia la descripcion automáticamente cuando nombre y edad cambian (computed)
+        `Su nombre es ${this.name()} y tiene ${this.age()} años`
     );
 
     getHeroDescription() {
         return this.heroDescription();
     }
 
-    //se utiliza el método privado para evitar harcodear la descripcion varias veces
-    private actualizaHeroDescription() { 
-        this.heroDescription.set(
-            `Su nombre es ${this.name} y tiene ${this.age} años`
-        );
-    }
-
     cambiarHero() {
-        this.name = 'Wolverine';
-        this.actualizaHeroDescription();
+        this.name.set('Wolverine');
     }
 
-    cambiarEdad () {
-        this.age = 45;
-        this.actualizaHeroDescription();
+    cambiarEdad() {
+        this.age.set(45);
     }
 
     reset() {
-        this.name = 'DeadPool';
-        this.age = 33;
-        this.actualizaHeroDescription();
+        this.name.set('DeadPool');
+        this.age.set(33);
     }
 
 }
