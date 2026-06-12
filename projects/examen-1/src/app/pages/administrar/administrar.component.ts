32fec115be9b942b75cc568023ca8126599b358a
Dennis EJ <albereli17@hotmail.com>
2026-06-12 13:16:59 -0600

    Creación de componentes

---
+++ projects/examen-1/src/app/pages/administrar/administrar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrar',
+  imports: [],
+  templateUrl: './administrar.component.html',
+  styleUrl: './administrar.component.css'
+})
+export class AdministrarComponent {
+
+}
---
+++ projects/examen-1/src/app/pages/saldos/saldos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saldos',
+  imports: [],
+  templateUrl: './saldos.component.html',
+  styleUrl: './saldos.component.css'
+})
+export class SaldosComponent {
+
+}
