148b6d2f36e1044a7c43c15a3e8c95d247cd7d89
Dennis EJ <albereli17@hotmail.com>
2026-06-25 06:41:17 -0600

    Semana-6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter.component'; //Se agrega la importación automáticamente
 import { HeroPageComponent } from './pages/hero/hero-page.component'
+import { AnimeComponent } from './pages/anime/anime-page.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'hero', //Se agrega el path para localHost:4200/hero
         component: HeroPageComponent //muestra el HeroPage con el path en la url
+    },
+    {
+        path: 'anime',
+        component: AnimeComponent
     }
 ];
